7b7293c7e98cc529aa1924fa15ae297f304dfbf2
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-16 15:17:53 -0400

    added business model

---
+++ app/models/businesses.py
@@ -0,0 +1,46 @@
+from sqlalchemy import func
+from .db import db
+
+class Business(db.Model):
+  __tablename__ = "businesses"
+
+  id = db.Column("id", db.Integer, primary_key = True)
+  name = db.Column("name", db.String, nullable=False)
+  address = db.Column("address", db.String, nullable=False)
+  description = db.Column("description", db.String, nullable=False)
+  url = db.Column("url", db.String, nullable=False)
+  phone = db.Column("phone", db.Integer, nullable=False)
+  state = db.Column("state", db.String, nullable=False)
+  city = db.Column("city", db.String, nullable=False)
+  zipcode = db.Column("zipcode", db.Integer, nullable=False)
+  lat = db.Column("lat", db.Float, nullable=False)
+  lng = db.Column("lng", db.Float, nullable=False)
+  open_time = db.Column("open_time", db.Time, nullable=False)
+  close_time = db.Column("close_time", db.Time, nullable=False)
+
+  created_at = db.Column("created_at", db.DateTime, default=func.now())
+  updated_at = db.Column("updated_at", db.DateTime, default=func.now(), on_update=func.now())
+
+  owner_id = db.Column("owner_id", db.Integer, db.ForeignKey("users.id"))
+
+  owner = db.relationship("User")
+
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "name": self.name,
+      "address": self.address,
+      "description": self.description,
+      "url": self.url,
+      "phone": self.phone,
+      "state": self.state,
+      "city": self.city,
+      "zipcode": self.zipcode,
+      "lat": self.lat,
+      "lng": self.lng,
+      "open_time": self.open_time.hour,
+      "close_time": self.close_time.hour,
+      "created_at": self.created_at,
+      "updated_at": self.updated_at,
+      "owner_id": self.owner_id,
+    }
