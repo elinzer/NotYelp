5cbe62deb54a79bde81f5b80ce634fe363b12f38
usernameGio <giordan.maniti@gmail.com>
2022-09-16 18:32:02 -0400

    working on delete/edit/create reviews

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,5 +1,5 @@
-from crypt import methods
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, request
+from flask_login import login_required, current_user
 from ..models import Review
 from ..models import db
 from ..forms.review_form import ReviewForm
@@ -23,14 +23,16 @@ def get_single(id):
 
 #Create a review
 @review_routes.route('/', methods=['POST'])
+@login_required
 def create_review():
     form = ReviewForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         new_review = Review(
                         stars=form.stars.data,
                         review=form.review_body.data,
                         business_id=form.business_id.data,
-                        user_id=form.user_id.data,
+                        user_id=current_user.id,
                         )
         db.session.add(new_review)
         db.session.commit()
@@ -40,9 +42,10 @@ def create_review():
 
 
 #Update Review
-@review_routes.route('/<id>', methods=['PUT'])
+@review_routes.route('/<int:id>', methods=['PUT'])
 def update_review(id):
     form = ReviewForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
     review = Review.query.get(id)
 
     if form.validate_on_submit():
@@ -52,3 +55,18 @@ def update_review(id):
         return review.to_dict()
     else:
         return {'errors': form.errors}, 422
+
+#Delete Review
+@review_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_review(id):
+    delete_review = Review.query.filter(Review.id == id).first()
+    if delete_review.user_id == current_user.id:
+        db.session.delete(delete_review)
+        db.session.commit()
+        return jsonify({
+        "message": "Business successfully deleted",
+        "status-code": 200
+    }), 200
+    else:
+        return {'Message': 'Unable to delete this review, This review belongs to another user.'}
