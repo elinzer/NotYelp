728fdc38a71b65bc316cbc7801dd87385e8bc342
El Linzer <elinzer5@gmail.com>
2022-09-16 16:00:04 -0500

    el started coding review routes

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,16 +1,18 @@
 from flask import Flask, Blueprint
 from ..models import Review
 
+
 review_routes = Blueprint('reviews', __name__)
 
 #Get all Reviews
 @review_routes.route('/')
 def get_all():
     reviews = Review.query.all()
-    return reviews.to_dict()
+    return { "reviews": [r.to_dict() for r in reviews] }
 
 #Get a single review
 
 @review_routes.route('/<id>')
-def get_single():
-    pass
+def get_single(id):
+    review = Review.query.filter(Review.id == id)
+    return review.to_dict()
