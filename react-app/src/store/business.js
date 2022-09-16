bf102ca363caad4b69c0f6b7651a2897e66bd480
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-16 17:37:27 -0400

    added create/edit thunks for business

--- react-app/src/store/business.js
+++ react-app/src/store/business.js
@@ -32,6 +32,36 @@ export const getBusinesses = () => async (dispatch) => {
   return res;
 };
 
+export const createBusiness = (business) => async (dispatch) => {
+  const res = await fetch("/api/businesses/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(business),
+  });
+  if (res.ok) {
+    const business = await res.json();
+    dispatch(create(business));
+  }
+  return res;
+};
+
+export const editBusiness = (business) => async (dispatch) => {
+  const res = await fetch(`/api/businesses/${business.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(business),
+  });
+  if (res.ok) {
+    const business = await res.json();
+    dispatch(update(business));
+  }
+  return res;
+};
+
 export const deleteBusinessById = (id) => async (dispatch) => {
   const res = await fetch(`/api/businesses/${id}`, {
     method: "DELETE",
@@ -49,6 +79,12 @@ export default function businessesReducer(state = {}, action) {
       action.payload.businesses.forEach((business) => {
         newState[business.id] = business;
       });
+      return newState;
+    case CREATE:
+      newState[action.payload.id] = action.payload;
+      return newState;
+    case UPDATE:
+      newState[action.payload.id] = action.payload;
       return newState;
     case DELETE:
       delete newState[action.payload];
