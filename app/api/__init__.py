9cf1eb6869f48420a1b920d7bbef06c3e735f924
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-16 16:44:15 -0400

    added review model migration

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,3 @@
 from .business_routes import business_routes
-from .review_routes import review
-from .user_routes import user
+from .review_routes import review_routes
+from .user_routes import user_routes
