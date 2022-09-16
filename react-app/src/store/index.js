6f6f50e250278efa5feffa2d3866bf8eb4a77494
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-16 17:21:20 -0400

    setup initial store sttae for get businesses

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,18 +1,18 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import session from './session'
-
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import session from "./session";
+import businessesReducer  from "./business";
 const rootReducer = combineReducers({
   session,
+  businesses: businessesReducer,
 });
-
 
 let enhancer;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require('redux-logger').default;
+  const logger = require("redux-logger").default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
