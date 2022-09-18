ed027b0fbe918b32c16b55fdb1cf02436da17ae0
edgar <edgarlee135@gmail.com>
2022-09-18 14:15:28 -0700

    edit business form

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -83,3 +83,4 @@ def delete_business(business_id):
       }), 200
   else:
     return {"errors": "Unauthorized"} , 401
+
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,7 +10,7 @@ import { authenticate } from "./store/session";
 import * as reviewActions from "./store/review";
 import BusinessCreateForm from "./components/Business/CreateBusiness";
 import CreateReview from './components/Reviews/CreateReviewModal';
-
+import BusinessEditForm from "./components/Business/EditBusiness";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -43,6 +43,9 @@ function App() {
         </Route>
         <Route path="/test-create-business">
           <BusinessCreateForm />
+        </Route>
+        <Route path="/test-edit-business">
+          <BusinessEditForm />
         </Route>
         <ProtectedRoute path='/users' exact={true} >
           <UsersList/>
