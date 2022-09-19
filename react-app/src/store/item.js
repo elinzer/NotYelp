c7f77bfb9042eecd67b3c09c1ea82307707ee6e9
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-19 12:26:56 -0400

    setup initial reducer for menu_items

---
+++ react-app/src/store/item.js
@@ -0,0 +1,72 @@
+const GET_ALL = "ITEMS/GET_ALL";
+const CREATE = "ITEMS/CREATE";
+const DELETE = "ITEMS/DELETE";
+
+const getAll = (items) => ({
+  type: GET_ALL,
+  payload: items,
+});
+
+const create = (item) => ({
+  type: CREATE,
+  payload: item,
+});
+
+const deleteItem = (itemId) => ({
+  type: DELETE,
+  payload: itemId,
+});
+
+export const getItems = () => async (dispatch) => {
+  const res = await fetch("/api/items/");
+  if (res.ok) {
+    const items = await res.json();
+    dispatch(getAll(items));
+  }
+  return res;
+};
+
+export const createItem = (item) => async (dispatch) => {
+  const res = await fetch("/api/items/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(item),
+  });
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(create(data));
+    return data;
+  } else if (res.status < 500) {
+    const data = await res.json();
+    if (data.errors) {
+      return data;
+    }
+  }
+  return ["An error occurred. Please try again."];
+};
+
+export const deleteItemById = (itemId) => async (dispatch) => {
+  const res = await fetch(`/api/items/${itemId}`, {
+    method: "DELETE",
+  });
+  if (res.ok) {
+    const data = await dispatch(deleteItem(itemId));
+    return data;
+  } else if (res.status < 500) {
+    const data = await res.json();
+    if (data.errors) {
+      return data;
+    }
+  }
+  return res;
+};
+
+export default function itemReducer(state = {}, action) {
+  let newState = { ...state };
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
