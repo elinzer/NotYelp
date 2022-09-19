8b82c21310171e9cb6d3ca507c6edf0b22d0a73b
edgar <edgarlee135@gmail.com>
2022-09-19 14:44:35 -0700

    createbutton style

--- react-app/src/components/Business/MenuItem/index.js
+++ react-app/src/components/Business/MenuItem/index.js
@@ -13,7 +13,7 @@ function CreateItemModal({ businessId }) {
 
   return (
     <>
-      <button className="createButton" onClick={() => setShowModal(true)}>
+      <button className="addMenuButton" onClick={() => setShowModal(true)}>
         Add Menu Item
       </button>
       {showModal && (
