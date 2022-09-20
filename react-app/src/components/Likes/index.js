5b5b69920d5761232e045b0acd095bb1b7f49aec
El Linzer <elinzer5@gmail.com>
2022-09-20 15:29:49 -0500

    pretty buggy like feature

--- react-app/src/components/Likes/index.js
+++ react-app/src/components/Likes/index.js
@@ -9,11 +9,21 @@ const LikeComponent = ({ business }) => {
     const sessionUser = useSelector(state => state.session.user)
     const likeState = useSelector(state => state.likes)
     const likes = Object.values(likeState)
-    console.log(likeState)
     const { id } = business
     const [clickedLove, setClickedLove] = useState(false)
     const [clickedOkay, setClickedOkay] = useState(false)
     const [clickedTrash, setClickedTrash] = useState(false)
+
+    //helper function to select 'like'
+    const likeSelected = () => {
+        let existingLike = likes.filter(like => {
+            if (like.user_id === sessionUser.id && like.business_id === id) {
+                return like;
+            }
+        })
+        let likeId = existingLike[0].id
+        return likeId
+    }
 
     useEffect(() => {
         dispatch(likeActions.getLikes())
@@ -21,24 +31,28 @@ const LikeComponent = ({ business }) => {
 
     useEffect(() => {
         setClickedLove(JSON.parse(window.localStorage.getItem('clickedLove')));
-      }, []);
+        setClickedOkay(JSON.parse(window.localStorage.getItem('clickedOkay')));
+        setClickedTrash(JSON.parse(window.localStorage.getItem('clickedTrash')));
+    }, []);
 
-      useEffect(() => {
+    useEffect(() => {
         window.localStorage.setItem('clickedLove', clickedLove);
-      }, [clickedLove]);
+    }, [clickedLove]);
 
-    const handleLove = (e) => {
-        e.preventDefault();
+    useEffect(() => {
+        window.localStorage.setItem('clickedOkay', clickedOkay);
+    }, [clickedOkay])
 
+    useEffect(() => {
+        window.localStorage.setItem('clickedTrash', clickedTrash);
+    }, [clickedTrash])
+
+    const handleLove = () => {
         if (clickedLove) {
             setClickedLove(!clickedLove)
-            let existingLike = likes.filter(like => {
-                if (like.user_id === sessionUser.id && like.business_id === id) {
-                    return like;
-                }
-            })
-            let likeId = existingLike[0].id
-            dispatch(likeActions.deleteLikeById(likeId))
+            if (likeSelected()) {
+                dispatch(likeActions.deleteLikeById(likeSelected()))
+            }
         } else {
             const info = {
                 like: 3,
@@ -52,40 +66,45 @@ const LikeComponent = ({ business }) => {
 
     }
 
-    const handleOkay = (e) => {
-        e.preventDefault();
+    const handleOkay = () => {
+        if (clickedOkay) {
+            setClickedOkay(!clickedOkay)
 
-        // if (clickedLove || clickedTrash) {
-        //     let existingLike = likes.filter(like => {
-        //         if (like.user_id === sessionUser.id && like.business_id === id) {
-        //             return like;
-        //         }
-        //     })
-        //     let likeId = existingLike[0].id
-        //     dispatch(likeActions.deleteLikeById(likeId))
+            if (likeSelected()) {
 
-        const info = {
-            like: 2,
-            user_id: sessionUser.id,
-            business_id: id
+                dispatch(likeActions.deleteLikeById(likeSelected()))
+            }
+        } else {
+            const info = {
+                like: 2,
+                user_id: sessionUser.id,
+                business_id: id
+            }
+
+            dispatch(likeActions.createLike(info))
+            setClickedOkay(!clickedOkay)
         }
 
-        dispatch(likeActions.createLike(info))
-        setClickedOkay(true)
-        // }
     }
 
-    const handleTrash = (e) => {
-        e.preventDefault();
+    const handleTrash = () => {
+        if (clickedTrash) {
+            setClickedTrash(!clickedTrash)
 
-        const info = {
-            like: 1,
-            user_id: sessionUser.id,
-            business_id: id
+            if (likeSelected()) {
+
+                dispatch(likeActions.deleteLikeById(likeSelected()))
+            }
+        } else {
+            const info = {
+                like: 1,
+                user_id: sessionUser.id,
+                business_id: id
+            }
+
+            dispatch(likeActions.createLike(info))
+            setClickedTrash(!clickedTrash)
         }
-
-        dispatch(likeActions.createLike(info))
-        setClickedTrash(true)
     }
 
 
@@ -94,17 +113,18 @@ const LikeComponent = ({ business }) => {
             <button
                 style={{ backgroundColor: clickedLove ? '#90EE90' : '' }}
                 className="love-button"
-                onClick={(e) => { setClickedLove(!clickedLove); handleLove(e) }}
-            // disabled={clickedLove}
+                onClick={() => { setClickedLove(!clickedLove); handleLove() }}
             >
                 <i class="fa-regular fa-face-grin-hearts"></i></button>
             <button
                 style={{ backgroundColor: clickedOkay ? '#F1BE48' : '' }}
-                onClick={handleOkay}>
+                onClick={() => { setClickedOkay(!clickedOkay); handleOkay() }}
+            >
                 <i class="fa-regular fa-face-meh"></i></button>
             <button
                 style={{ backgroundColor: clickedTrash ? '#FF7276' : '' }}
-                onClick={handleTrash}>
+                onClick={() => { setClickedTrash(!clickedTrash); handleTrash() }}
+            >
                 <i class="fa-regular fa-face-frown"></i></button>
 
 
