1fec42b6065d5a57b42842c3b110e1a863d70dce
edgar <edgarlee135@gmail.com>
2022-09-20 10:14:46 -0700

    delete menuitem

--- react-app/src/components/Business/ItemCard/index.js
+++ react-app/src/components/Business/ItemCard/index.js
@@ -1,14 +1,38 @@
+import { useSelector, useDispatch } from "react-redux";
+import {useHistory, useParams } from "react-router-dom";
+import { getBusinessByid } from "../../../store/business";
+import { deleteItemById } from "../../../store/item";
+
 import "./ItemCard.css";
 
+
 function ItemCard({ item }) {
+  const { itemId } = useParams();
+  // const items = useSelector(state => state.items[itemId])
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    await dispatch(deleteItemById(item.id));
+    await dispatch(getBusinessByid(item.business_id))
+  };
+
   return (
     <div className="item-card">
       <div className="item-card-image">
-        <img className="item-image" src={item.preview_image} />
+        <img className="item-image" src={item?.preview_image} />
       </div>
       <div className="item-card-info">
-        <div className="item-card-name">{item.name}</div>
-        <div className="item-card-price">${item.price}</div>
+        <div className="item-card-name">{item?.name}</div>
+        <div className="item-card-price">${item?.price}</div>
+        <div>
+        <button
+         onClick={handleDelete}
+         className="menuitem delete">
+         Delete Item
+        </button>
+        </div>
       </div>
     </div>
   );
