b5215ba11065a66673f16368ae8bd0caf43c32ba
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-21 14:08:50 -0400

    fetch businesses on splash page load

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import BusinessCard from "../Business/BusinessCard";
+import { getBusinesses } from "../../store/business";
 import "./SplashPage.css";
 function SplashPage() {
   const dispatch = useDispatch();
   const businesses = useSelector((state) => Object.values(state.businesses));
+
+  useEffect(() => {
+    dispatch(getBusinesses());
+  }, [dispatch]);
   // https://s3-media0.fl.yelpcdn.com/educatorphoto/T3U8OES-w0Pat6ijyqf8pg/o.jpg
   return (
     <div className="main-splash-container">
