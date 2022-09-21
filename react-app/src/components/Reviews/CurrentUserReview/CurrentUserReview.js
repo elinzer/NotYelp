7bb8a56c96a2e0fd7f724ec5156461a5057a2225
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-21 15:40:01 -0400

    styling changes...

--- react-app/src/components/Reviews/CurrentUserReview/CurrentUserReview.js
+++ react-app/src/components/Reviews/CurrentUserReview/CurrentUserReview.js
@@ -4,6 +4,7 @@ import "./CurrentUserReview.css";
 import * as reviewActions from "../../../store/review";
 import EditReviewModal from "../EditReviewModal";
 import DisplayStars from "../DisplayStars";
+import ReviewCard from "../ReviewCard";
 
 const CurrentUserReviews = () => {
   const user = useSelector((state) => state.session.user);
@@ -20,41 +21,15 @@ const CurrentUserReviews = () => {
     dispatch(reviewActions.getReviews());
   }, []);
 
-  const handleDeleteButton = (review_id) => {
-    dispatch(reviewActions.deleteReviewById(review_id));
-  };
-
   return (
     <div className="currentReviewContainer">
       <h2 className="MyReviewHeader">My Reviews</h2>
       <div className="my-review">
-        {filteredReviews.map((review, i) => (
-          <div className="review-card-current" key={`${review.id} ${i + 1}`}>
-            <div className="user-info">
-              <img
-                className="review-user-image-current"
-                src={review?.user.profile_image}
-              />
-              <div className="users-name-current">
-                <span>{`${user.username}`}</span>
-                <span className="span-review-stars">
-                  <DisplayStars rating={review.stars} />
-                </span>{" "}
-              </div>
-            </div>
-
-            <div className="review-current">{`Review ${i + 1}: ${
-              review.review
-            }`}</div>
-            <button
-              className="RevButton"
-              onClick={() => handleDeleteButton(review.id)}
-            >
-              Delete Review
-            </button>
-            <EditReviewModal rev={review} />
-          </div>
-        ))}
+        <div className="my-review-inner">
+          {filteredReviews.map((review, i) => (
+            <ReviewCard review={review} />
+          ))}
+        </div>
       </div>
     </div>
   );
