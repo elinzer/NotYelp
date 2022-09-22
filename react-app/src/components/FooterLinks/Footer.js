e5fa6f11be78671ae1380ba4ee9f900607fb930a
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-22 16:15:08 -0400

    adjusted styling of the footer

--- react-app/src/components/FooterLinks/Footer.js
+++ react-app/src/components/FooterLinks/Footer.js
@@ -7,46 +7,54 @@ function FooterAbout() {
     <footer className="footer">
       <div className="footer-container">
         <span>
+          GitHub Repository - Not Yelp
           <a href="https://github.com/elinzer/NotYelp">
-            <i className="fab fa-github" />
-            GitHub Repository - Not Yelp
+            <i className="fab fa-github fa-2xl" />
           </a>
         </span>
         <span>
-          <a href="https://github.com/hazeluwuz">
-            <i className="fab fa-github" />
-            Hazel Guzzetti
-          </a>
-          <a href="https://www.linkedin.com/in/hazel-guzzetti-50445b23b/">
-            <i className="fab fa-linkedin" />
-          </a>
+          Hazel Guzzetti
+          <div className="dev-links">
+            <a href="https://github.com/hazeluwuz">
+              <i className="fab fa-github fa-2xl" />
+            </a>
+            <a href="https://www.linkedin.com/in/hazel-guzzetti-50445b23b/">
+              <i className="fab fa-linkedin fa-2xl" />
+            </a>
+          </div>
         </span>
         <span>
-          <a href="https://github.com/EdgarMLee">
-            <i className="fab fa-github" />
-            Edgar Lee
-          </a>
-          <a href="https://www.linkedin.com/in/edgar-lee-1357el135/">
-            <i className="fab fa-linkedin" />
-          </a>
+          Edgar Lee
+          <div className="dev-links">
+            <a href="https://github.com/EdgarMLee">
+              <i className="fab fa-github fa-2xl" />
+            </a>
+            <a href="https://www.linkedin.com/in/edgar-lee-1357el135/">
+              <i className="fab fa-linkedin fa-2xl" />
+            </a>
+          </div>
         </span>
         <span>
-          <a href="https://github.com/elinzer">
-            <i className="fab fa-github" />
-            El Linzer
-          </a>
-          <a href="https://www.linkedin.com/in/elinzer/">
-            <i className="fab fa-linkedin" />
-          </a>
+          El Linzer
+          <div className="dev-links">
+            <a href="https://github.com/elinzer">
+              <i className="fab fa-github fa-2xl" />
+            </a>
+            <a href="https://www.linkedin.com/in/elinzer/">
+              <i className="fab fa-linkedin fa-2xl" />
+            </a>
+          </div>
         </span>
         <span>
-          <a href="https://github.com/giordanferda">
-            <i className="fab fa-github" />
-            Giordan Maniti
-          </a>
-          <a href="https://www.linkedin.com/in/giordanmaniti/">
-            <i className="fab fa-linkedin" />
-          </a>
+          Giordan Maniti
+          <div className="dev-links">
+            <a href="https://github.com/giordanferda">
+              <i className="fab fa-github fa-2xl" />
+            </a>
+            <a href="https://www.linkedin.com/in/giordanmaniti/">
+              <i className="fab fa-linkedin fa-2xl" />
+            </a>
+          </div>
         </span>
       </div>
     </footer>
